a5cdb8c44505552c4612bb00dde8da1d33f74270
lmrazuki <69096596+lmrazuki@users.noreply.github.com>
2021-01-10 20:15:12 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask import Response,json
-from flask_cors import CORS, cross_origin
+# from flask_cors import CORS, cross_origin
 from flask import Flask, render_template, request, redirect
 
 from pymongo import MongoClient
